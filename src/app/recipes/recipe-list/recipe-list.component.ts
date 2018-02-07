929efe5a1df8955b5ee3c8766665a376e70d7fd0
Brett Jenken <brettjenken@hotmail.com>
2018-02-06 20:11:31 -0500

    Added property and event binding to the recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model'; 
 
@@ -8,13 +8,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMbWO9U-A6_v_pVeeDYJgmYmXl0RQLafzNs9jYo-df_abwaWm2Xg'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMbWO9U-A6_v_pVeeDYJgmYmXl0RQLafzNs9jYo-df_abwaWm2Xg')
+    new Recipe('A Test Recipe 1', 'This is a test 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMbWO9U-A6_v_pVeeDYJgmYmXl0RQLafzNs9jYo-df_abwaWm2Xg'),
+    new Recipe('A Test Recipe 2', 'This is a test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMbWO9U-A6_v_pVeeDYJgmYmXl0RQLafzNs9jYo-df_abwaWm2Xg')
   ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  // recipeSelected(rec : Recipe){
+  //   console.log(rec);
+  // }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from './recipe.model';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  currRecipe : Recipe;
   constructor() { }
 
   ngOnInit() {
   }
+
+  updateRecipe(rec : Recipe){
+    this.currRecipe = rec;
+    console.log(rec);
+  }
+
 
 }
