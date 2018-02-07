89bba24d95bd3969361cc3b4bad134dabc165c14
Brett Jenken <brettjenken@hotmail.com>
2018-02-06 20:33:50 -0500

    Lecture 81: Used property binding + event binding to implement basic add feature of the shopping list

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from "../../shared/ingredient.model";
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInput : ElementRef;
+  @ViewChild('amountInput') amountInput : ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addIngredients(){
+    this.ingredientAdded.emit(new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value));
   }
 
 }
